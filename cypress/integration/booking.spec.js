2ed5065801d0c20aa4c01834c12c21e1501b1506
edsonMark <edson@101ways.com>
2019-06-07 13:49:29 +0100

    update test

--- cypress/integration/booking.spec.js
+++ cypress/integration/booking.spec.js
@@ -7,16 +7,19 @@ describe('Create Booking', () => {
     before(() => {
         cy.visit('http://hotel-test.equalexperts.io/')
         cy.url().should('include', 'equalexperts.io')   
-        cy.title().should('equal', 'Hotel booking form')
     });
     
     it('should create booking with Valid Credentials', () => {
-        cy.get(booking.firstNameInput).scrollIntoView()
-        cy.get(booking.newBooking)
-
+        // cy.ge(t(booking.firstNameInput).scrollIntoView()
+        cy.scrollTo('bottom')
+        cy.get(booking.firstNameInput).type('Homer')
+        cy.get(booking.lastNameInput).type('Simpson')
+        cy.get(booking.bookingAmountField).type('600')
         cy.get(booking.bookingCheckIn).type('2019-07-10')
         cy.get(booking.bookingCheckOut).type('2019-07-30')
         cy.get('#form').find("[type='button']").click()
-        // cy.get('#bookings').should('include', 'Ken')
+    })
+
+    it('should delete a booking', () => {
     })
 })
